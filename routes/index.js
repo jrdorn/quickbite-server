33ba187ada082f0709b411dc5ff89b2d5bfee99b
Josh Dorn <jr_dorn@yahoo.com>
2022-01-31 19:54:08 -0600

    Add default styles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* getAddress */
+router.get("/get-address", function (req, res, next) {
+  res.render("index", { title: "getAddress" });
+});
+
+/* getCoords */
+router.get("/get-coords", function (req, res, next) {
+  res.render("index", { title: "getCoords" });
+});
+
+/* validateAddress */
+router.get("/validate-address", function (req, res, next) {
+  res.render("index", { title: "validateAddress" });
+});
+
+/* findRestaurants */
+router.get("/find-restaurants", function (req, res, next) {
+  res.render("index", { title: "findRestaurants" });
+});
+
+/* sendMail */
+router.get("/send-mail", function (req, res, next) {
+  res.render("index", { title: "sendMail" });
 });
 
 module.exports = router;
