eaf6836f5dc8fd122e0305d947ba04affa504669
Josh Dorn <jr_dorn@yahoo.com>
2022-02-10 20:02:41 -0600

    Return coords directly

--- routes/getAddress.js
+++ routes/getAddress.js
@@ -2,20 +2,27 @@ const fetch = require("node-fetch");
 
 //reverse geocoding - lookup address given lat/lng
 const getAddress = (coords) => {
-  return new Promise((resolve, reject) => {
-    fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
-      {
-        method: "post",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => resolve(res.json()))
-      .catch((err) => reject(err));
-  });
+  return coords;
+  // return new Promise((resolve, reject) => {
+  //   fetch(
+  //     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
+  //     {
+  //       method: "post",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json, err) => {
+  //       if (err) {
+  //         reject(err);
+  //       } else {
+  //         resolve(json);
+  //       }
+  //     });
+  // });
 };
 
 module.exports = getAddress;
