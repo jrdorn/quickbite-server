093f130bdbc91800cefde1d2f0f84c66c8b805cb
Josh Dorn <jr_dorn@yahoo.com>
2022-02-10 20:38:01 -0600

    Revert normal fetch request for getAddress

--- routes/getAddress.js
+++ routes/getAddress.js
@@ -2,27 +2,26 @@ const fetch = require("node-fetch");
 
 //reverse geocoding - lookup address given lat/lng
 const getAddress = (coords) => {
-  return JSON.stringify(coords);
-  // return new Promise((resolve, reject) => {
-  //   fetch(
-  //     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
-  //     {
-  //       method: "post",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //     }
-  //   )
-  //     .then((res) => res.json())
-  //     .then((json, err) => {
-  //       if (err) {
-  //         reject(err);
-  //       } else {
-  //         resolve(json);
-  //       }
-  //     });
-  // });
+  return new Promise((resolve, reject) => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
+      {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json, err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(json);
+        }
+      });
+  });
 };
 
 module.exports = getAddress;
