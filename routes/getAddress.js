3abd304003c9d0913e4a2435bc84f2f06199ad02
Josh Dorn <jr_dorn@yahoo.com>
2022-02-11 20:26:21 -0600

    Implement getAddress with async/await

--- routes/getAddress.js
+++ routes/getAddress.js
@@ -1,21 +1,37 @@
 const fetch = require("node-fetch");
 
 //reverse geocoding - lookup address given lat/lng
-const getAddress = (coords) => {
-  return new Promise((resolve, reject) => {
-    fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
-      {
-        method: "post",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((json) => resolve(json));
-  });
+// const getAddress = (coords) => {
+//   return new Promise((resolve, reject) => {
+//     fetch(
+//       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
+//       {
+//         method: "post",
+//         headers: {
+//           Accept: "application/json",
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     )
+//       .then((res) => res.json())
+//       .then((json) => resolve(json));
+//   });
+// };
+
+// module.exports = getAddress;
+
+const getAddress = async (coords) => {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${process.env.MAPS_KEY}`,
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.json();
 };
 
 module.exports = getAddress;
